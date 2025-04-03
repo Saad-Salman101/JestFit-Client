72ac04f6ffc28d17672b47fcfac1a39bcd6079dd
saad <saad.salman98@gmail.com>
2025-04-04 00:37:32 +0500

    initial commit

---
+++ src/app/pricing/page.js
@@ -0,0 +1,13 @@
+// components
+// import PricingView from 'src/sections/pricing/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Pricing',
+};
+
+export default function PricingPage() {
+  // return <PricingView />;
+  return <div>Pricing</div>;
+}
